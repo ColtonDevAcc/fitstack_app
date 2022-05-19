83544019991ed8bd6ba1fe7f6d4f1334f530f637
CordPowell <cpowell@hrcorrosion.com>
2022-05-18 21:03:21 -0500

    Changes to Dashboard View

--- lib/features/dashboard/presentation/dashboard_view.dart
+++ lib/features/dashboard/presentation/dashboard_view.dart
@@ -18,67 +18,68 @@ class _DashBoard_ViewState extends State<DashBoard_View> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "HOW YOUR DAY LOOKS?",
-                    style: Theme.of(context).textTheme.headline3,
+    return SingleChildScrollView(
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: AppBar().preferredSize.height + 20,
+                ),
+                Text(
+                  "HOW YOUR DAY LOOKS?",
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Stats",
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                Statistics_Dashboard(),
+                Text(
+                  "Daily Workouts",
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: PageView.builder(
+                    clipBehavior: Clip.none,
+                    pageSnapping: false,
+                    controller: _pageController,
+                    onPageChanged: (index) {
+                      setState(() {
+                        currIndex = index;
+                      });
+                    },
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Transform.translate(
+                        offset: Offset(-80, 0),
+                        child: AnimatedScale(
+                          scale: index == currIndex ? 1.05 : 0.9,
+                          child: WorkoutCard(),
+                          duration: Duration(milliseconds: 200),
+                        ),
+                      );
+                    },
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Stats",
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                  Statistics_Dashboard(),
-                  Text(
-                    "Daily Workouts",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    height: 250,
-                    width: double.infinity,
-                    child: PageView.builder(
-                      clipBehavior: Clip.none,
-                      pageSnapping: false,
-                      controller: _pageController,
-                      onPageChanged: (index) {
-                        setState(() {
-                          currIndex = index;
-                        });
-                      },
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 10,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Transform.translate(
-                          offset: Offset(-80, 0),
-                          child: AnimatedScale(
-                            scale: index == currIndex ? 1.05 : 0.9,
-                            child: WorkoutCard(),
-                            duration: Duration(milliseconds: 200),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Text("Progress"),
-                  Container(
-                    height: 200,
-                    width: 100,
-                    color: Colors.grey[300],
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+                ),
+                Text("Progress"),
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.grey[300],
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
