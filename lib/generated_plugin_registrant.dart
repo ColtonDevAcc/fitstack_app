685a700c8e6cefc9e8f944e76d034f784594eca1
Colton Bristow <cbristow99@gmail.com>
2022-06-05 11:42:50 -0500

    added all firebase things

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,12 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:device_info_plus_web/device_info_plus_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:firebase_performance_web/firebase_performance_web.dart';
+import 'package:firebase_remote_config_web/firebase_remote_config_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -14,6 +20,12 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   DeviceInfoPlusPlugin.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FirebasePerformanceWeb.registerWith(registrar);
+  FirebaseRemoteConfigWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:fitstackapp/core/routing/appRouter.gr.dart';
 import 'package:fitstackapp/core/theme/theme.dart';
 import 'package:fitstackapp/features/login/presentation/login_view.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'core/theme/color_Theme.dart';
 
-void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => MyApp(), // Wrap your app
-      ),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
 
   final List views = const [
     Login_View(),
@@ -30,7 +47,11 @@ class MyApp extends StatelessWidget {
         ? MaterialApp.router(
             theme: lightTheme,
             routerDelegate: _appRouter.delegate(
-              navigatorObservers: () => [AutoRouteObserver()],
+              navigatorObservers: () => [
+                AutoRouteObserver(),
+                observer,
+                FirebaseAnalyticsObserver(analytics: analytics),
+              ],
             ),
             routeInformationParser: _appRouter.defaultRouteParser(),
             routeInformationProvider: _appRouter.routeInfoProvider(),
@@ -41,7 +62,13 @@ class MyApp extends StatelessWidget {
             locale: DevicePreview.locale(context),
             builder: DevicePreview.appBuilder,
             theme: lightTheme,
-            routerDelegate: _appRouter.delegate(),
+            routerDelegate: _appRouter.delegate(
+              navigatorObservers: () => [
+                AutoRouteObserver(),
+                observer,
+                FirebaseAnalyticsObserver(analytics: analytics),
+              ],
+            ),
             routeInformationParser: _appRouter.defaultRouteParser(),
             routeInformationProvider: _appRouter.routeInfoProvider(),
           );
