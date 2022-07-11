0684f602aeb4cdac850f0e4a05bf66b03add3ae0
Colton Bristow <cbristow4@uco.edu>
2022-07-11 15:44:20 -0500

    fixed button animation if index != 0

--- lib/presentation/signup/presentation/molecules/social_auth_buttons.dart
+++ lib/presentation/signup/presentation/molecules/social_auth_buttons.dart
@@ -12,29 +12,32 @@ class Social_Auth_Buttons extends StatelessWidget {
     return BlocBuilder<SignupCubit, SignupState>(
       builder: (context, state) {
         return AnimatedContainer(
-          duration: Duration(milliseconds: 900),
+          duration: Duration(milliseconds: 400),
+          curve: Curves.decelerate,
           //TODO make this size the size of the login row for auth buttons
           height: state.index == 0 ? MediaQuery.of(context).size.height * .045 : 0,
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SocialAuthButton_Widget(
-                color: Theme.of(context).colorScheme.primary,
-                child: FaIcon(
-                  FontAwesomeIcons.facebookF,
-                  color: Theme.of(context).colorScheme.onPrimary,
+              if (state.index == 0)
+                SocialAuthButton_Widget(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: FaIcon(
+                    FontAwesomeIcons.facebookF,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
                 ),
-              ),
-              SizedBox(width: 15),
-              SocialAuthButton_Widget(
-                onTap: () {},
-                color: Theme.of(context).colorScheme.secondary,
-                child: FaIcon(
-                  FontAwesomeIcons.google,
-                  color: Theme.of(context).colorScheme.onSecondary,
+              if (state.index == 0) SizedBox(width: 15),
+              if (state.index == 0)
+                SocialAuthButton_Widget(
+                  onTap: () {},
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: FaIcon(
+                    FontAwesomeIcons.google,
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
                 ),
-              ),
             ],
           ),
         );
