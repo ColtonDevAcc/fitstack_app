530d68f568b02c9863579585460a73df5652332f
ColtonDevAcc <cbristow99@gmail.com>
2022-10-27 20:31:42 -0500

    quickfix for user profiles

--- lib/app/repository/auth_repository.dart
+++ lib/app/repository/auth_repository.dart
@@ -13,7 +13,7 @@ class AuthenticationRepository {
   final controller = StreamController<AuthStream>.broadcast();
   final Dio dio = Dio();
   final storage = new FlutterSecureStorage();
-  static String mainUrl = kDebugMode ? "http://10.0.2.2:8000" : "https://dev.fitstack.io";
+  static String mainUrl = kDebugMode ? "http://localhost:8000" : "https://dev.fitstack.io";
 
   AuthenticationRepository({
     fb.FirebaseAuth? firebaseAuth,
